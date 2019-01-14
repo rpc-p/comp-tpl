5459818e93cc07e7b84833405a8646266ed884c0
moonlit <moonlitusun@gmail.com>
2019-01-14 10:18:27 +0800

    ts react模板

---
+++ template/example/scripts/app.jsx
@@ -0,0 +1,22 @@
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
+//
+import './app.scss';
+
+export default class App extends PureComponent {
+  static propTypes = {
+
+  };
+
+  static defaultProps = {
+
+  }
+
+  state = {
+
+  }
+
+  render() {
+    return null;
+  }
+}
